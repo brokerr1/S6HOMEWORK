dcc33db267229d37a9e7d1790cdde39f3ce86f4c
brokerr1 <solovievdima1@gmail.com>
2022-05-30 19:19:47 +0300

    2

--- 2/Program.cs
+++ 2/Program.cs
@@ -6,3 +6,16 @@ x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
 b1 = 2, k1 = 5, b2 = 4, k2 =
 9 -> (-0,5; 5,5)
 */
+Console.WriteLine("Введите значения переменных уравнения (y = k1 * x + b1)");
+Console.Write("b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значения переменных уравнения (y = k2 * x + b2) для второй прямой");
+Console.Write("b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+y = k1 * x + b1;
+y = k2 * x + b2;
+//осталось понять все остальное
