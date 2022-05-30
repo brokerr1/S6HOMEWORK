eb234eff8b3fec7d336982fcbac80781f46d0ed2
brokerr1 <solovievdima1@gmail.com>
2022-05-30 19:02:51 +0300

    1

--- 1/Program.cs
+++ 1/Program.cs
@@ -4,3 +4,22 @@
 0, 7, 8, -2, -2 -> 2
 -1, -7, 567, 89, 223-> 3
 */
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Сформированный массив:");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("{0} ", array[i]);
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество чисел больше нуля:{count}");
